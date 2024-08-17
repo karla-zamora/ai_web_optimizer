b51f8007b2e20d66a0b48716a713d842999e313e
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-17 13:33:34 -0700

    Add start of main page and toolbar/header

---
+++ app/components/StartButton.js
@@ -0,0 +1,7 @@
+export default function StartButton() {
+    return (
+      <button className="px-4 py-2 rounded-md border border-neutral-300 bg-white text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+        Get Started
+      </button>
+    )
+}

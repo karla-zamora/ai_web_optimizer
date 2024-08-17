4f70ed6dbd62190d331124a22659b13ecda383e7
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-17 13:43:49 -0700

    Add section 2 and restyle

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,9 +2,9 @@
 export default function Header(){
     return (
         <header>
-            <nav class="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+            <nav class="bg-white border-gray-600 border-solid border-b-2 px-6 lg:px-6 py-2.5 dark:bg-gray-800">
                 <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-                    <a href="/" class="flex items-center">
+                    <a href="/#landing" class="flex items-center">
                         <img src="https://www.svgrepo.com/show/375811/flame.svg" class="mr-3 h-6 sm:h-9" alt="Logo" />
                         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">EmberCraft</span>
                     </a>
@@ -23,7 +23,7 @@ export default function Header(){
                                 <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white" aria-current="page">Dashboard</a>
                             </li>
                             <li>
-                                <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
+                                <a href="/#features" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
                             </li>
                             <li>
                                 <a href="#" class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
--- app/page.js
+++ app/page.js
@@ -6,7 +6,8 @@ export default function Home(){
   return (
     <div class="flex flex-col h-screen">
       <Header />
-      <div class="grid h-100 grid-cols-12 gap-4 flex-col mx-auto">
+
+      <div class="grid bg-white h-100 grid-cols-12 gap-4 flex-col mx-auto" id="landing">
         <div class="p-4 col-span-2"/>
         <div class="p-4 col-span-3 text-center flex flex-col justify-center">
           <h1 class="text-3xl p-6">Crafting harmony<br/> in every digital moment.</h1>
@@ -22,6 +23,23 @@ export default function Home(){
           <img style={{borderRadius: '20px'}} src="https://images.unsplash.com/photo-1459664018906-085c36f472af?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         </div>
       </div>
+
+      <div class="grid bg-white h-100 grid-cols-12 gap-4 flex-col mx-auto" id="features">
+        <div class="p-4 col-span-6 justify-center flex flex-col">
+          <img style={{borderRadius: '20px'}} src="https://images.unsplash.com/photo-1459664018906-085c36f472af?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
+        </div>
+        <div class="p-4 col-span-1"/>
+        <div class="p-4 col-span-3 text-center flex flex-col justify-center">
+          <h1 class="text-3xl p-6">Crafting harmony<br/> in every digital moment.</h1>
+          <br/>
+          <div class="border-solid border-2 border-white-500">
+            <h4>We are not just a product - <br/> we are your other half to come up <br/> with a design that is truly yours.</h4>
+          </div>
+          <br/>
+          <div class="p-6"><StartButton /></div>
+        </div>
+      </div>
+      <div class="p-4 col-span-2"/>
     </div>
   );
 }

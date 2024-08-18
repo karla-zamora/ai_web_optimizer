1e444c127a8a80c42444e79f13b316a99e8da9c4
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-17 17:17:06 -0700

    Add pricing and footer to main page

--- app/components/BasicButton.js
+++ app/components/BasicButton.js
@@ -1,8 +1,8 @@
-export default function StartButton() {
+export default function BasicButton() {
     return (
       <button className="px-4 py-2 rounded-md border border-neutral-300 bg-white text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
-        <a href="/#pricing">
-          Get Started
+        <a href="/#">
+          Choose Basic
         </a>
       </button>
     )

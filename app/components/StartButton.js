3dce61441d469f791b5c6251cfbdbee6daab565e
enjiwood <87221016+enjiwood@users.noreply.github.com>
2024-08-17 20:04:08 -0700

    Add button styling

--- app/components/StartButton.js
+++ app/components/StartButton.js
@@ -1,6 +1,6 @@
 export default function StartButton() {
     return (
-      <button className="px-4 py-2 rounded-md border border-neutral-300 bg-white text-neutral-500 text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-md">
+      <button className="px-4 py-2 rounded-md border border-[#d49c73] bg-[#c75c5c] text-[#f3cb83] font-bold text-sm hover:-translate-y-1 transform transition duration-200 hover:shadow-[#f3cb83] hover:shadow-md">
         <a href="/#pricing">
           Get Started
         </a>

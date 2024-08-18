ec93fa6fbd03f3dbd295edb1c31c212db4d7c6d7
Jonathan B <31107158+jballo@users.noreply.github.com>
2024-08-17 19:49:25 -0700

    Added basic sidebar to dashboard

--- app/dashboard/[[...dashboard]]/page.js
+++ app/dashboard/[[...dashboard]]/page.js
@@ -2,6 +2,22 @@
 import Header from '@/app/components/Header';
 import DashboardLayout from '../../components/Dashboardlayout';
 import Footer from '@/app/components/Footer';
+import { Sidebar, SidebarBody, SidebarLink } from '@/app/components/ui/sidebar';
+
+import {
+  IconArrowLeft,
+  IconBrandTabler,
+  IconSettings,
+  IconUserBolt,
+  IconHome
+} from "@tabler/icons-react";
+
+// Example usage:
+const links = [
+  { label: "Home", href: "/", icon: <IconHome /> },
+  { label: "Settings", href: "/about", icon: <IconSettings /> },
+  { label: "Logout", href: "/logout", icon: <IconUserBolt /> }
+];
 
 export default function Page() {
   return (
@@ -9,7 +25,23 @@ export default function Page() {
       <Header/>
 
       <DashboardLayout>
-        <h1>Welcome to your dashboard!</h1>
+        <div className="flex flex-row h-full w-full">
+          <div className="rounded-tl-2xl bg-gray-200 h-full">
+            <Sidebar>
+              <SidebarBody className="">
+                {links.map((link) => (
+                  <SidebarLink key={link.href} link={link} />
+                ))}
+              </SidebarBody>
+            </Sidebar>
+          </div>
+          <div className="flex-grow p-4">
+            <h1>Welcome to your dashboard!</h1>
+            <p>Some content</p>
+
+          </div>
+        </div>
+
       </DashboardLayout>
 
       <Footer />
